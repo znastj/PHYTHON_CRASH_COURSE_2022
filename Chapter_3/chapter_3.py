fea95ddc1c4694d9459a40c4038d1594abff8a96
znastj <znastj@gmail.com>
2022-08-30 20:53:16 +0300

    chapter-3

---
+++ Chapter_3/chapter_3.py
@@ -0,0 +1,7 @@
+visitors = ['Vadik', 'Sveta', 'Andriy']
+message = visitors[0] + ", please, visit my lunch. See you!"
+print(message)
+
+popped_motorcycle = motorcycles.pop() 
+print(motorcycles)
+print(popped_motorcycle)
